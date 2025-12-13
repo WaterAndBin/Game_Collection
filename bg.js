f55f63d497250c7ab651bd21d203d65ea8c17a22
WaterAndBin <1336263157@qq.com>
2025-12-13 20:39:37 +0800

    feat: 添加敌人

--- bg.js
+++ bg.js
@@ -5,7 +5,7 @@ const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 
 // 游戏速度
-let gameSpeed = 15;
+let gameSpeed = 20;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "./background/layer-1.png";
@@ -32,28 +32,44 @@ class Layer {
   update() {
     this.speed = gameSpeed * this.speedModifier;
     if (this.x <= -this.width) {
-      this.x = this.width + this.x2 - this.speed;
-    }
-    if (this.x2 <= -this.width) {
-      this.x2 = this.width + this.x - this.speed;
+      this.x = 0;
     }
     this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
-
-    console.log(this.x2);
   }
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
-const layer1 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.1);
+const layer2 = new Layer(backgroundLayer2, 0.3);
+const layer3 = new Layer(backgroundLayer3, 0.5);
+const layer4 = new Layer(backgroundLayer4, 0.6);
+const layer5 = new Layer(backgroundLayer5, 0.8);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   layer1.update();
   layer1.draw();
+
+  layer2.update();
+  layer2.draw();
+
+  layer3.update();
+  layer3.draw();
+
+  layer4.update();
+  layer4.draw();
+
+  layer5.update();
+  layer5.draw();
 
   requestAnimationFrame(animate);
 }
